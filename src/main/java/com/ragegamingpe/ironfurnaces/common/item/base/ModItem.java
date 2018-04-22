7785c023d910151710b3ecb372db53b1f0e6b488
Dominic Carrington <carringtondominic@gmail.com>
2018-04-22 09:47:46 +0100

    Added base code for Items

--- src/main/java/com/ragegamingpe/ironfurnaces/common/item/base/ModItem.java
+++ src/main/java/com/ragegamingpe/ironfurnaces/common/item/base/ModItem.java
@@ -1,10 +1,35 @@
 package com.ragegamingpe.ironfurnaces.common.item.base;
 
 import com.ragegamingpe.ironfurnaces.client.model.ModelLoader;
+import com.ragegamingpe.ironfurnaces.common.lib.LibMisc;
+import com.ragegamingpe.ironfurnaces.common.lib.ModItems;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class ModItem extends Item implements IModItem
 {
+    public ModItem(String regName)
+    {
+        super();
+
+        this.setUnlocalizedName(regName);
+        this.setRegistryName(LibMisc.MOD_ID, regName);
+        ModItems.ALL_ITEMS.add(this);
+    }
+
+    @Override
+    public String getUnlocalizedName()
+    {
+        return String.format("item.%s:%s", LibMisc.MOD_ID, getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    public ModItem setCreativeTab(CreativeTabs tab)
+    {
+        super.setCreativeTab(tab);
+        return this;
+    }
+
     @Override
     public void registerRender()
     {
